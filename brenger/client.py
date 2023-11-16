db1b6aed817e514e7a04f891abf23bbac044df82
Omer <omer@whoppah.com>
2023-11-16 13:21:11 +0100

    Encoded to utf-8

--- brenger/client.py
+++ brenger/client.py
@@ -22,7 +22,7 @@ class BrengerAPIClient:
 
     def create_shipment(self, shipment_data: ShipmentCreateRequest) -> ShipmentResponse:
         url = BASE_URL.format(namespace=self.namespace) + "/shipments"
-        response = self.session.post(url, data=shipment_data.model_dump_json())
+        response = self.session.post(url, data=shipment_data.model_dump_json().encode("utf-8"))
         self._handle_response_errors(response)
         logger.info(
             "Shipment created successfully with ID: %s", response.json().get("id")
